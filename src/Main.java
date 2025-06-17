dc4e2053491fede029ed2fdeb0dd3ddea0976f85
이은정 <ej2grateful@gmail.com>
2025-06-17 10:15:13 +0900

    array, stream

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] color = {"빨", "주", "노", "초", "파", "남", "보", "흰"};
+
+        //배열의 경우 for 보다는 stream 사용
+        for(String c: color) {
+            System.out.println("color : " + c);
+        }
+
+        Arrays.stream(color).forEach(n -> System.out.println(n));
+
+    }
+}
